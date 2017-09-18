6976bb912b1c09b7643d88e78dfa9b638cbba08b
Takafumi Kimura <meta.matryoshka@gmail.com>
2017-09-18 18:30:24 +0900

    2017/09/18 #2

--- Enjoy Swift.playground/Pages/vol2.xcplaygroundpage/Contents.swift
+++ Enjoy Swift.playground/Pages/vol2.xcplaygroundpage/Contents.swift
@@ -1,5 +1,31 @@
 //: [Previous](@previous)
+//: **Optionalを判定する(??)**
+var nickName: String? = "Taka"
+let fullName = "takafumi kimura"
 
+var unformalMessage = "Hi!" + (nickName ?? fullName)
+
+print(unformalMessage)
+
+nickName = nil
+var formalMessage = "Hi!" + (nickName ?? fullName)
+print(formalMessage)
+//: **Switch文**
+// SwiftではInt以外の値もcase判定で使えます
+// default: の指定は必須
+
+let enginnerType = "iOS Developer"
+
+switch enginnerType {
+case "iOS Developer":
+    print("モバイルは世界を変える")
+case "Infra Enginner":
+    print("インフラはいつの時代でも必要")
+case "Web Developer":
+    print("オープンイノベーション")
+default:
+    print("Technorogy change the world")
+}
 
 
 //: [Next](@next)
