e2637b7b54302a0b35a09e03c14045bb534a6a44
木村昂史 <10005188+takafumi-kimura@users.noreply.github.com>
2018-01-20 19:41:08 +0900

    2018/01/20 クラスについて #4

--- Enjoy Swift.playground/Pages/vol4.xcplaygroundpage/Contents.swift
+++ Enjoy Swift.playground/Pages/vol4.xcplaygroundpage/Contents.swift
@@ -89,19 +89,70 @@ var cicle = Circle(name: "ball", radius: 5)
 cicle.area()
 
 
+// getter,setterを追加する
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0.0
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+    
+    var perimeter: Double {
+        get {
+            return 3.0 * sideLength
+        }
+        set {
+            sideLength = newValue / 3.0
+        }
+    }
+    
+    override func simpleDescription() -> String {
+        return "An equilateral triangle with sides of length \(sideLength)."
+    }
+}
+var triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
+print(triangle.perimeter)
+triangle.perimeter = 9.9
+print(triangle.sideLength)
+
+// 呼び出されるまでプロパティのセットを必要としない場合はwillSetを使う
+// 以下のクラスでは、三角形の横幅と四角形の横幅は常に同じとなる
+
+class TriangleAndSquare {
+    var triangle: EquilateralTriangle {
+        willSet {
+            square.sideLength = newValue.sideLength
+        }
+    }
+    var square: Square {
+        willSet {
+            triangle.sideLength = newValue.sideLength
+        }
+    }
+    init(size: Double, name: String) {
+        square = Square(sideLength: size, name: name)
+        triangle = EquilateralTriangle(sideLength: size, name: name)
+    }
+}
+
+var triangleAndSquare = TriangleAndSquare(size: 10, name: "another test shape")
+print(triangleAndSquare.square.sideLength)
+print(triangleAndSquare.triangle.sideLength)
+triangleAndSquare.square = Square(sideLength: 50, name: "larger square")
+print(triangleAndSquare.triangle.sideLength)
 
 
 
 
+// カスタムクラスのインスタンス変数も、他のクラスと同様にoptionalを取ることができる。
+// もしも値がnilだった場合は、"?"以降の処理は無視される
+// => nil.sideLengthが呼び出されてしまい、nil落ちするということはない
+// optionalを使って導き出された値はすべてoptionalな値となる。 => "sideLength"はoptional
 
-
-
-
-
-
-
-
-
-
-
+var optionalSquare: Square? = Square(sideLength: 2.5, name: "optional square")
+var sideLength = optionalSquare?.sideLength
+optionalSquare = nil
+sideLength = optionalSquare?.sideLength
 //: [Next](@next)
